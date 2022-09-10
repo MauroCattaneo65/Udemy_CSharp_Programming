cbf5a43e4e29b93e704ed37583b1dc54563a2f4e
MauroCattaneo65 <112676863+MauroCattaneo65@users.noreply.github.com>
2022-09-10 06:39:41 +0200

    Add files via upload

---
+++ IntegralTypes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IntegralTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 500;
+            short b = 100;
+            sbyte c = -50;
+            long d = 100_000_000L;
+            int e = 0b10101010;
+            ushort f = 0x0A56;
+
+            //Formattazione utilizzando una stringa di formato - con indici numerici
+            Console.WriteLine("a={0} b={1} c={2} d={3} e={4} f={5}",a,b,c,d,e,f);
+
+            //Formattazione utilizzando l'interpolazione di stringhe
+            Console.WriteLine($"a={a} b={b} c={c} d={d} e={e} f={f}");
+        }
+    }
+}
