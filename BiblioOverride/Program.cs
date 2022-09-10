423e712f1e180a89c98ae3221f9bff7c70a105fe
MauroCattaneo65 <112676863+MauroCattaneo65@users.noreply.github.com>
2022-09-10 07:24:55 +0200

    Add files via upload

---
+++ BiblioOverride/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Biblio
+{
+    public class Persona
+    {
+        public string Nome {get;set;}
+        public string Cognome {get;set;}
+        public virtual string Denominazione 
+        {
+            get 
+            {
+                return Nome + " " + Cognome;
+            }
+        }
+    }
+    public class Utente : Persona
+    {
+        public string Id {get; set;}
+
+        public int AnnoIscrizione {get;set;}
+
+        public override string Denominazione 
+        {
+            get 
+            {
+                return Id + " * " + Nome + " * " + Cognome;
+            }
+        }
+
+        public Utente()
+        {
+
+        }
+    }
+
+    public class Libro
+    {
+        private string id;
+        private string titolo;
+        private string autore;
+        private Utente utente;
+
+        public Libro(string id, string titolo, string autore)
+        {
+            this.id = id;
+            this.titolo = titolo;
+            this.autore = autore;
+            this.utente = null;
+        }
+
+        public string Descrizione()
+        {
+            return this.titolo + " di " + this.autore;
+        }
+
+        public void Prestito(Utente utente)
+        {
+            if(this.utente != null)
+            {
+                Console.WriteLine($"Libro già in prestito a: {this.utente.Denominazione}");
+            }
+            else
+            {
+                this.utente = utente;
+                Console.WriteLine($"Libro prestato a: {this.utente.Denominazione}");
+            }
+     
+        }
+
+        public void Restituzione()
+        {
+            if(this.utente != null)
+            {
+                 Console.WriteLine($"L'utente: {utente.Denominazione} ha restituito il libro.");
+            }
+            this.utente = null;
+        }
+    }
+  public  class Biblioteca
+    {
+        public static void Main(string[] args)
+        {
+            Utente utente1 = new Utente() {Id = "001", Nome = "Mario", Cognome = "Rossi", AnnoIscrizione=2020};
+            Console.WriteLine(utente1.Denominazione);
+
+            Utente utente2 = new Utente() {Id = "002", Nome = "Giuseppe", Cognome = "Verdi", AnnoIscrizione=2010};
+            Console.WriteLine(utente2.Denominazione);
+
+            Libro libro = new Libro("1","L'isola misteriosa","Giulio Verne");
+            Console.WriteLine(libro.Descrizione());
+
+            libro.Prestito(utente1);
+            libro.Prestito(utente2);
+            libro.Restituzione();
+            libro.Prestito(utente2);
+        }
+    }
+}
---
+++ EsempioFattoriale/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EsempioFattoriale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Intodurre un numero intero:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{n}! = {Fattoriale(n)}");
+        }
+
+        static int Fattoriale(int n)
+        {
+            if(n == 0)
+                return 1;
+            return n*Fattoriale(n-1);
+        }
+    }
+}
