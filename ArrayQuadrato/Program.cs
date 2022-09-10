9ab5eb320ac20ff6cf78ee14020a6229fa5ee4bf
MauroCattaneo65 <112676863+MauroCattaneo65@users.noreply.github.com>
2022-09-10 07:09:27 +0200

    Add files via upload

---
+++ ArrayQuadrato/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ArrayQuadrato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[5,5];
+            int counter = ;
+            for(int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    counter++;
+                    arr[i,j] = (j == 0) ? 0 : counter;
+                    Console.Write($"{arr[i,j]} ");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
---
+++ Asterischi/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Asterischi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            int righe;
+            
+            while(true)
+            {
+                Console.WriteLine("Inserisci un numero ('no' per uscire): ");
+
+                s = Console.ReadLine();
+                
+                if(s.ToLower() == "no") 
+                    break;
+
+                righe = Convert.ToInt32(s);
+
+                for(int r = 1; r <= righe; r++)
+                {
+                    for(int ast = 0; ast <= r; ast++)
+                    {
+                        Console.Write("* ");
+                    }
+                
+                   Console.Write("\n");
+                }
+
+            }
+
+            Console.WriteLine("The end!");
+        }
+    }
+}
