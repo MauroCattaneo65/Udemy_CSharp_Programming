e1ecb69220d16e2eb2e84a19c6cbbd8263a63fce
MauroCattaneo65 <112676863+MauroCattaneo65@users.noreply.github.com>
2022-09-10 07:10:51 +0200

    Add files via upload

---
+++ Condizioni/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Condizioni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+
+            Console.WriteLine("Scrivi qualcosa: ");
+            s = Console.ReadLine();
+
+            if(s == "primo")
+            { 
+                Console.WriteLine("Hai scritto primo");
+            } 
+            else if(s == "secondo")
+            {
+                Console.WriteLine("Hai scritto secondo");
+            }
+            else
+            {
+                Console.WriteLine($"Hai scritto {s}");
+            }
+
+            switch(s)
+            {
+                case "primo":
+                Console.WriteLine("Hai scritto primo");
+                break;
+                case "secondo":
+                Console.WriteLine("Hai scritto secondo");
+                break;
+                default:
+                Console.WriteLine($"Hai scritto {s}");
+                break;
+            }
+        }
+    }
+}
